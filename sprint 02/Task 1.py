1d34574ca09cb185fd55630f169b4361a1751111
Ilya <grynyshyn080@mail.com>
2021-07-27 17:24:18 +0300

    Add Task2

---
+++ sprint 02/Task 1.py
@@ -0,0 +1,8 @@
+def double_string(data):
+    count=0
+    for x in data:
+        if any(x+y in data for y in data):
+            count+=1
+    return count
+data = ['aa', 'aaaa', 'aaaaaaaa', 'aaaa', 'qwer', 'qwerqwert']
+print(double_string(data))
