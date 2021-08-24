5b06507673db8cf4a77c6e3b2d26021cc5004f51
Ilya <grynyshyn080@gmail.com>
2021-08-25 01:17:22 +0300

    add

--- Task 04.js
+++ Task 04.js
@@ -6,8 +6,20 @@ function factorial(n) {
        return n * (n-1);
    }
 }
-console.log(factorial(5))
+// console.log(factorial(5))
 
 // function processArray(arr, factorial) {
-//     // your code
+//     return arr.map(function(el){return factorial(el)});
 // }
+//
+// console.log(processArray([1,2,4,5,6],5))
+
+function pr(arr,factorial) {
+    let ab = [];
+    for ( let i=0; i<arr.length; i+=1 ) {
+        return ab.push(arr[i]);
+    }
+    return factorial(ab)
+}
+
+console.log(pr([1,3,4,5,6,7],factorial))
